a97de44ca9cd58c703b7641a09c93f54f224b5be
Robert Griesel <r.griesel@gmail.com>
2020-02-03 17:43:30 +0100

    year in about was wrong

--- dialogs/about/about.py
+++ dialogs/about/about.py
@@ -41,7 +41,7 @@ class AboutDialog(Dialog):
         self.view.set_modal(True)
         self.view.set_program_name('Porto')
         self.view.set_version('0.0.1')
-        self.view.set_copyright('Copyright © 2017-2012 - the Porto developers')
+        self.view.set_copyright('Copyright © 2017-2020 - the Porto developers')
         self.view.set_comments('Porto is a notebook style interface to Python and SageMath. It is designed to make exploring mathematics easy and fun.')
         self.view.set_license_type(Gtk.License.GPL_3_0)
         self.view.set_website('https://www.cvfosammmm.org/porto/')
